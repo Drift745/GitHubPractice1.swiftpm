bf097411afe6ea2239fdc79aa72be3ec9fdadc5e
Drift745 <cbrown4049@stu.d214.org>
2023-10-03 07:48:32 -0500

    Headerview

---
+++ HeaderView.swift
@@ -0,0 +1,25 @@
+//
+//  HeaderView.swift
+//  GitHubPractice1
+//
+//  Created by Cody Brown on 9/27/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+
+struct HeaderView:View{
+    @Binding var Tasks:[Task]
+    @State var newTask = ""
+    var body: some View{
+        HStack{
+            TextField("enter new task",text:$newTask)
+            Button("add task"){
+                Tasks.append("\($newTask)")
+            }
+        }
+    }
+}
